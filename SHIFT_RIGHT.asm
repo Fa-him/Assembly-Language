8090894a5908aad01076b1b6bea94008126200c9
Md Fahim Bin Alam <125723764+Fa-him@users.noreply.github.com>
2025-09-28 10:35:49 +0600

    Add files via upload

---
+++ SHIFT_RIGHT.asm
@@ -0,0 +1,45 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    NUM DB 2
+    MSG_1 DB 0DH, 0AH, 'NUMBER BEFORE SHR: $'
+    MSG DB 0DH, 0AH, 'RESULT AFTER SHR: $'
+    CRLF DB 13,10,'$'
+   
+.CODE 
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    MOV DX, OFFSET MSG_1
+    MOV AH, 09H
+    INT 21H
+    
+    MOV DL, NUM
+    ADD DL, '0'
+    MOV AH, 02H
+    INT 21H
+    
+    MOV DX, OFFSET MSG
+    MOV AH, 09H
+    INT 21H
+    
+    MOV AL, NUM
+    
+    MOV CL, 2
+    SHR AL, CL
+    
+    ADD AL, '0'
+    MOV DL, AL
+    MOV AH, 02H
+    INT 21H
+    
+    MOV DX, OFFSET CRLF
+    MOV AH, 09H
+    INT 21H
+    
+    MOV AH, 4CH
+    INT 21H
+    
+MAIN ENDP 
+END MAIN
